1085c170b4db77dea7f2c9cabcf8f262d8d2c11f
inatan <inatan_hertzog@hotmail.com>
2021-01-05 21:16:34 -0300

    first commit

---
+++ Alura.WebAPI.DAL.Livros/LeituraContext.cs
@@ -0,0 +1,30 @@
+﻿using Alura.ListaLeitura.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alura.ListaLeitura.Persistencia
+{
+    public class LeituraContext : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+
+
+        public LeituraContext(): base()
+        {
+            //irá criar o banco e a estrutura de tabelas necessárias
+            this.Database.EnsureCreated();
+        }
+
+        public LeituraContext(DbContextOptions<LeituraContext> options) 
+            : base(options)
+        {
+            //irá criar o banco e a estrutura de tabelas necessárias
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration<Livro>(new LivroConfiguration());
+        }
+    }
+}
